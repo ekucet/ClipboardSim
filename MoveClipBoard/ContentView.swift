78042a6690fcd930f8ee34cdfe8059877acabfe6
Erkam Kucet <ekucet@gmail.com>
2026-04-03 10:31:50 +0300

    Initial Commit

---
+++ MoveClipBoard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoveClipBoard
+//
+//  Created by Erkam Kucet on 3.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoveClipBoard/MoveClipBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveClipBoardApp.swift
+//  MoveClipBoard
+//
+//  Created by Erkam Kucet on 3.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MoveClipBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
